002d1ce8e7426c994fa29258b294d317669b13ea
Martin Rumánek <martin@rumanek.cz>
2014-07-08 14:35:56 +0200

    Fix starting ssh server

--- dockerfile/init.sh
+++ dockerfile/init.sh
@@ -4,10 +4,12 @@ if [ ! -d "/imageserver-data/.ssh" ]; then
   mkdir /imageserver-data/.ssh
   chown imageserver:imageserver /imageserver-data/.ssh
   chmod 700 /imageserver-data/.ssh
-  su -l imageserver -c "ssh-keygen -t rsa -N \"\" -f ~/.ssh/id_rsa"
+  su -l imageserver -c "ssh-keygen -t rsa -N \"\" -f /imageserver-data/.ssh/id_rsa"
 fi
 
-/etc/init.d/apache2 start;
+su -l imageserver -c "cat /imageserver-data/.ssh/id_rsa.pub > ~/.ssh/authorized_keys"
+
+/etc/init.d/apache2 start; /etc/init.d/ssh start
 
 while :; do /bin/bash; sleep 1; done
 
